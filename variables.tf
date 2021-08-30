0da3a6756ff3b89206982fb87bd990599b09ba26
tgremlin <allantenison@live.com>
2021-08-30 16:10:30 -0500

    changed variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-locals {
-  groups = {
+variable "groups" {
+  default = {
     example0 = {
       description = "sg description 0"
     },
@@ -7,25 +7,35 @@ locals {
       description = "sg description 1"
     }
   }
-
-  rules = {
+}
+variable "rules" {
+  default = {
     example0 = [{
-      description = "rule description 0",
-      port = 80,
-      cidr_blocks = ["10.0.0.0/16"],
+      description      = "rule description 0"
+      to_port          = 80
+      from_port        = 80
+      cidr_blocks      = ["10.0.0.0/16"]
     },{
-      description = "rule description 1",
-      port = 81,
-      cidr_blocks = ["10.1.0.0/16"],
+      description      = "rule description 1"
+      to_port          = 80
+      from_port        = 80
+      cidr_blocks      = ["10.1.0.0/16"]
     }]
-    example1 = [{
-      description = "rule description 0",
-      port = 80,
-      cidr_blocks = ["10.2.0.0/16"],
-    },{
-      description = "rule description 1",
-      port = 81,
-      cidr_blocks = ["10.3.0.0/16"],
-    }]
+    example1 = [] # empty Array removes rules
+  }
+}
+locals {
+  rules = {
+    for k,v in var.rules:
+      k => [
+        for i in v:
+        merge({
+          ipv6_cidr_blocks = null
+          prefix_list_ids  = null
+          security_groups  = null
+          protocol         = "tcp"
+          self             = null
+        }, i)
+      ]
   }
 }
